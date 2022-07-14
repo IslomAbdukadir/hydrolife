1e45099c1a4b4b795fd69566ccd20d48d5510ca0
islomabduqodir <islomabduqodir@gmail.com>
2022-07-14 15:38:58 +0500

    resp

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
     // DOM ready
     // If a link has a dropdown, add sub menu toggle.
     $("nav ul li a:not(:only-child)").click(function (e) {
+      list2.style.right = "-150px";
+
       $(this).siblings(".nav-dropdown").toggle();
       // Close one dropdown when selecting another
       $(".nav-dropdown").not($(this).siblings()).hide();
@@ -16,25 +18,43 @@
   }); // end DOM ready
 })(jQuery);
 
+//
 
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  });
+});
 
 //
 
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+let span = document.querySelector(".header__span");
+let list = document.querySelector(".header__list");
+let list2 = document.querySelector(".header__list2");
+let line = document.querySelector(".line");
 
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener("click", event => {
+$(".header__span").click(function () {
+  span.style.display = "none";
+  list.style.right = "0";
+  list2.style.right = "0";
+  line.style.right = "0";
+});
 
-    accordionItemHeader.classList.toggle("active");
-    const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if(accordionItemHeader.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    }
-    else {
-      accordionItemBody.style.maxHeight = 0;
-    }
-    
-  });
+$(".exit").click(function () {
+  span.style.display = "flex";
+  list.style.right = "-150px";
+  list2.style.right = "-150px";
+  line.style.right = "-150px";
 });
 
 //
